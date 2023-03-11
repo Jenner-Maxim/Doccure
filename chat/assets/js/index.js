8752ae2ca7539d30681f29932765e1911f57e601
Maxim <jennersi1remaxim@gmail.com>
2023-03-11 23:11:59 +0300

    Done with html and css front-end for v1

---
+++ chat/assets/js/index.js
@@ -0,0 +1,5 @@
+var person = document.querySelector(".chat .all-contact .person");
+person.scrollTop = person.scrollHeight - person.clientHeight;
+
+var chat = document.querySelector(".chat-message .chat");
+chat.scrollTop = chat.scrollHeight - chat.clientHeight;
